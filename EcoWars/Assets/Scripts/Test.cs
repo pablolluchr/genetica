3137843f9f492715020dfdc1b07b2719bbec401f
Pablo Lluch <pablolluch@Pablos-MBP.lan>
2020-04-25 00:42:16 +0100

    debugging enabled

---
+++ EcoWars/Assets/Scripts/Test.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("This is a test message");
+        int test = 1; //Debugger should stop here
+        Debug.Log("This shouldn't be printed until debugger resumes");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
