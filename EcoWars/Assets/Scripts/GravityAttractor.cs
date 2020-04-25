c53f4fd2ebd779cd88210dac39f9872bd466f6e5
Pablo Lluch <pablolluch@Pablos-MBP.lan>
2020-04-25 20:43:03 +0100

    Implement planet-based gravity and target-based movement of units

---
+++ EcoWars/Assets/Scripts/GravityAttractor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//objects that create gravity should use this script (planets)
+public class GravityAttractor : MonoBehaviour
+{
+    public float gravity = -10f;
+
+    //nake body stand up on planet (force + rotation)
+    public void Attract(Transform body){
+        Vector3 targetDir = (body.position-transform.position).normalized; //center of the planet
+        Vector3 bodyUP = body.up;
+
+        //transition to the target direction (pointing to the center of the planet)
+        body.rotation = Quaternion.FromToRotation(bodyUP,targetDir) * body.rotation;
+
+        //Apply force in the direction of the earth 
+        body.GetComponent<Rigidbody>().AddForce(targetDir*gravity);
+    }
+}
---
+++ EcoWars/Assets/Scripts/UnitMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//Handles the target-based movement of units
+[RequireComponent(typeof(Rigidbody))]
+public class UnitMovement : MonoBehaviour
+{
+
+    public Transform target; //unit moves towards target
+    public float minDistance = 1f;//stops moving when minDistance reached
+    public float maxDistance = 6f; //only follow targets maxDistance appart
+    public float speed;
+    private Rigidbody rb;
+    private GravityAttractor planet;
+
+
+    public void Awake()
+    {
+        planet = GameObject.FindGameObjectWithTag("Planet").GetComponent<GravityAttractor>();
+        GetComponent<Rigidbody>().useGravity = false; //deactivate built-in downwards gravity
+        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 moveDir = target.position - rb.position;
+        moveDir = Vector3.ProjectOnPlane(moveDir, transform.up); //movement only tangent to the planet
+
+        if (moveDir.magnitude > minDistance && moveDir.magnitude < maxDistance) //target in reach and not too close
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(moveDir, Vector3.up);
+
+            //only change rotation of y axis
+            transform.rotation = targetRotation;
+
+            //move forward in the local axis
+            rb.MovePosition(rb.position + transform.forward * Time.fixedDeltaTime * speed);
+        }
+
+        planet.Attract(transform);
+    }
+}
